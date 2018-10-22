ec3c12b324a5962c31f19427c3c28630a8656614
professorX <malicia.rg@gmail.com>
2018-10-22 20:47:17 +0200

    for pull

---
+++ src/com/malicia/mrg/photo/app/phototri/PhotoTri/Progress.java
@@ -0,0 +1,22 @@
+package com.malicia.mrg.photo.app.phototri.PhotoTri;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
+public class Progress {
+
+    @FXML
+    private ProgressBar progressB;
+
+    @FXML
+    private Text textUp;
+
+    @FXML
+    private Text textMiddle;
+
+    @FXML
+    private TextArea TextBox;
+
+}
